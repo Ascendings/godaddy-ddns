35c6df97c355d6ead66b19ade6ead4ecf8ed8f53
Ascendings <brotherballantine@gmail.com>
2015-08-21 14:28:02 -0400

    Updated Logger class

--- godaddy_ddns/lib/Logger.py
+++ godaddy_ddns/lib/Logger.py
@@ -19,7 +19,7 @@ class Logger:
 	def setupLogfile(self):
 		level = self.getLogLevel()
 		if not level:
-			print('{0} is not a supported log level. Supported levels are INFO and DEBUG.'.format(self.config['level']))
+			print('{0} is not a supported log level. You can view https://docs.python.org/3/library/logging.html#logging-levels for supported logging levels.'.format(self.config['level']))
 			sys.exit()
 	
 		logging.basicConfig(filename = self.config['file'], format = '%(asctime)s %(message)s', level = level)
@@ -41,4 +41,29 @@ class Logger:
 		return {
 			'info': logging.INFO,
 			'debug': logging.DEBUG,
-		}.get(log_level, False)
+			'critical': logging.CRITICAL,
+			'error': logging.ERROR,
+			'warning': logging.WARNING,
+			'notset': logging.NOTSET,
+		}.get(log_level.lower(), False)
+		
+	def debug(self, msg, *args, **kwargs):
+		logging.debug(msg, *args, **kwargs)
+	
+	def info(self, msg, *args, **kwargs):
+		logging.info(msg, *args, **kwargs)
+	
+	def warning(self, msg, *args, **kwargs):
+		logging.warning(msg, *args, **kwargs)
+	
+	def critical(self, msg, *args, **kwargs):
+		logging.critical(msg, *args, **kwargs)
+		
+	def error(self, msg, *args, **kwargs):
+		logging.error(msg, *args, **kwargs)
+	
+	def log(self, lvl, msg, *args, **kwargs):
+		logging.log(lvl, msg, *args, **kwargs)
+	
+	def exception(self, msg, *args, **kwargs):
+		logging.exception(msg, *args, **kwargs)
