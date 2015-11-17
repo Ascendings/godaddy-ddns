dbfd6e9a49996d95680d179e8873a7a74a671832
Ascendings <brotherballantine@gmail.com>
2015-11-17 16:34:08 -0500

    Fixed syntax error and updated version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'GoDaddy Dynamic DNS script,
+	'description': 'GoDaddy Dynamic DNS script',
 	'author': 'Ascendings',
 	'url': 'https://github.com/Ascendings/godaddy-ddns',
 	'download_url': 'https://github.com/Ascendings/godaddy-ddns/archive/master.zip',
 	'author_email': 'brotherballantine@gmail.com',
-	'version': '0.3.0',
+	'version': '0.7.0',
 	'install_requires': ['nose', 'pygodaddy'],
 	'packages': ['godaddy-ddns', 'python3', 'python3-yaml'],
 	'scripts': [],
