26f79c3df81599c278355eacf59375d4e01af4fd
Ascendings <brotherballantine@gmail.com>
2015-11-17 16:34:36 -0500

    Fixed class variable scope issues

--- godaddy_ddns/lib/GoDaddy.py
+++ godaddy_ddns/lib/GoDaddy.py
@@ -23,19 +23,19 @@ class GoDaddy:
 	 
 		for domain in self.client.find_domains():
 			for dns_record in self.client.find_dns_records(domain):
-				logger.debug("Domain '{0}' DNS records: {1}".format(domain, self.client.find_dns_records(domain)))
+				self.logger.debug("Domain '{0}' DNS records: {1}".format(domain, self.client.find_dns_records(domain)))
 				# only update the bluewolf subdomain
 				if dns_record.hostname == self.config['hostname']:
 					if public_ip != dns_record.value:
 						if self.client.update_dns_record(dns_record.hostname + "." + domain, public_ip):
-							logger.info("Host '{0}' public IP set to '{1}'".format(dns_record.hostname, public_ip))
+							self.logger.info("Host '{0}' public IP set to '{1}'".format(dns_record.hostname, public_ip))
 							# update our local copy of IP
-							network.writeIpFile(self.config, public_ip)
+							self.network.writeIpFile(public_ip)
 						else:
-							logger.info("Failed to update Host '{0}' IP to '{1}'".format(dns_record.hostname, public_ip))
+							self.logger.info("Failed to update Host '{0}' IP to '{1}'".format(dns_record.hostname, public_ip))
 					else:
-						logger.info("Nothing was changed")
-						network.writeIpFile(self.config, public_ip)
+						self.logger.info("Nothing was changed")
+						self.network.writeIpFile(public_ip)
 				else:
-					logger.info("Not {0}: '{1}', skipping".format(self.config['hostname'], dns_record.hostname))
+					self.logger.info("Not {0}: '{1}', skipping".format(self.config['hostname'], dns_record.hostname))
 		return
